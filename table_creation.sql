9fc92e4ca833f599b2422d51590ffbe8eac9956a
Brian Rodgers Vargo <47480412+bethanrv@users.noreply.github.com>
2019-10-13 16:57:28 -0400

    Update table_creation.sql
    Added identity tags where applicable
    Added content field to group post
    Added userID field to group event

--- table_creation.sql
+++ table_creation.sql
@@ -2,117 +2,118 @@
 use master
 
 /* Create hobiDB if none exists*/
-If not exists(
-				select name from master.dbo.sysdatabases
-				where name = 'HobiDB'
-			 )
-Begin
-create database [HobiDB]
-End
 
-use HobiDB
+Begin try
+	create database [HobiDB]
+	use HobiDB
+End try
+begin catch
+	use HobiDB
+end catch
+
  
 
 
-
-
-
-
 CREATE TABLE UserLogin (
-	userID int NOT NULL,
-	userName varchar(255),
-	passHash varchar(255),
-	PRIMARY KEY (userID)
+userID int PRIMARY KEY IDENTITY,
+userName varchar(255),
+passHash varchar(255),
 );
 
 CREATE TABLE UserInterests (
-	userID int,
-	interestTag varchar(255),
-	PRIMARY KEY (userID, interestTag),
-	FOREIGN KEY (userID) REFERENCES UserLogin(userID)
+userID int,
+interestTag varchar(255),
+PRIMARY KEY (userID, interestTag),
+FOREIGN KEY (userID) REFERENCES UserLogin(userID)
 );
 
 CREATE TABLE UserInfo (
-	email varchar(255),
-	fName varchar(255),
-	lName varchar(255),
-	userID int,
-	PRIMARY KEY (email, userID),
-	FOREIGN KEY (userID) REFERENCES UserLogin(userID)
+email varchar(255),
+fName varchar(255),
+lName varchar(255),
+userID int,
+PRIMARY KEY (email, userID),
+FOREIGN KEY (userID) REFERENCES UserLogin(userID)
 );
 
 CREATE TABLE UserImage (
-	userImage varbinary(max),
-	userID int,
-	PRIMARY KEY (userID),
-	FOREIGN KEY (userID) REFERENCES UserLogin(userID)
+userImage varbinary(max),
+userID int,
+PRIMARY KEY (userID),
+FOREIGN KEY (userID) REFERENCES UserLogin(userID)
 );
 
 CREATE TABLE GroupInfo (
-	groupID int NOT NULL,
-	groupName varchar(255),
-	groupDescription varchar(255),
-	groupLocation varchar(255), 
-	isPrivate bit,
-	PRIMARY KEY (groupID)
+groupID int NOT NULL Identity,
+groupName varchar(255),
+groupDescription varchar(255),
+groupLocation varchar(255), 
+isPrivate bit,
+PRIMARY KEY (groupID)
 );
 
 CREATE TABLE GroupImage (
-	groupImage varbinary(max),
-	groupID int,
-	PRIMARY KEY (groupID),
-	FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID)
+groupImage varbinary(max),
+groupID int,
+PRIMARY KEY (groupID),
+FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID)
 );
 
 CREATE TABLE GroupInterests (
-	groupID int,
-	groupInterestTag varchar(255),
-	PRIMARY KEY (groupID, groupInterestTag),
-	FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID)
+groupID int,
+groupInterestTag varchar(255),
+PRIMARY KEY (groupID, groupInterestTag),
+FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID)
 );
 
 
 CREATE TABLE GroupPost (
-    groupPostID int,
-	groupID int,
-	userID int,
-	postTime datetime,
-	PRIMARY KEY (groupPostID),
-	FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID),
-	FOREIGN KEY (userID) REFERENCES UserLogin(userID)
+groupPostID int IDENTITY,
+groupID int,
+userID int,
+postTime datetime,
+postContent varchar(255),
+PRIMARY KEY (groupPostID),
+FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID),
+FOREIGN KEY (userID) REFERENCES UserLogin(userID)
 );
+
 
 CREATE TABLE GroupEvent (
-    groupEventID int NOT NULL,
-	groupID int,
-	creationTimestamp datetime,
-	scheduledTimestamp datetime,
-	description varchar(max),
-	PRIMARY KEY (groupEventID, groupID),
-	FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID)
+groupEventID int NOT NULL IDENTITY,
+groupID int,
+userID int,
+creationTimestamp datetime,
+scheduledTimestamp datetime,
+description varchar(max),
+PRIMARY KEY (groupEventID, groupID),
+FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID),
+foreign key (userID) REFERENCES UserLogin(userID)
 );
 
+
 CREATE TABLE RSVPUser (
-	userID int,
-	groupEventID int,
-    groupID int,
-    PRIMARY KEY (userID, groupEventID, groupID),
-	FOREIGN KEY (userID) REFERENCES UserLogin(userID),
-	FOREIGN KEY (groupEventID, groupID) REFERENCES GroupEvent(groupEventID, groupID)
+userID int,
+groupEventID int,
+groupID int,
+PRIMARY KEY (userID, groupEventID, groupID),
+FOREIGN KEY (userID) REFERENCES UserLogin(userID),
+FOREIGN KEY (groupEventID, groupID) REFERENCES GroupEvent(groupEventID, groupID)
 );
 
 CREATE TABLE GroupUsers (
-	userID int,
-	groupID int,
-	PRIMARY KEY (userID, groupID),
-	FOREIGN KEY (userID) REFERENCES UserLogin(userID),
-	FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID)
+userID int,
+groupID int,
+PRIMARY KEY (userID, groupID),
+FOREIGN KEY (userID) REFERENCES UserLogin(userID),
+FOREIGN KEY (groupID) REFERENCES GroupInfo(groupID)
 );
 
 CREATE TABLE GroupPostLikes (
-	groupPostID int,
-	userID int,
-	PRIMARY KEY (groupPostID, userID),
-	FOREIGN KEY (groupPostID) REFERENCES GroupPost(groupPostID),
-	FOREIGN KEY (userID) REFERENCES UserLogin(userID)
+groupPostID int ,
+userID int,
+PRIMARY KEY (groupPostID, userID),
+FOREIGN KEY (groupPostID) REFERENCES GroupPost(groupPostID),
+FOREIGN KEY (userID) REFERENCES UserLogin(userID)
 );
+
