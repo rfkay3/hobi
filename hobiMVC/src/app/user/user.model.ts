8d5fe53e43ab521f1c6285022dc8e07e18da5945
rfkay3 <rfkay3@gmail.com>
2019-12-07 14:39:45 -0500

    Add Base User Model

---
+++ hobiMVC/src/app/user/user.model.ts
@@ -0,0 +1,10 @@
+// Exports a model class for a user object
+export class User {
+    id: Number;
+    user_name: String;
+    interest_tags: Array<String>;
+    email: String;
+    first_name: String;
+    last_name: String;
+    user_image: any;
+}
---
+++ hobiMVC/src/app/user/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  // create webmethod object to obtain user info
+  constructor() { }
+}
